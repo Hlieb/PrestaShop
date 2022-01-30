bbfd4f0965781c610fc0a110828eff70792cf2aa
Hlieb <hliebboiechko@gmail.com>
2022-01-30 16:38:48 +0100

    ProductPage changed

--- src/main/java/pages/ChosenProductPage.java
+++ src/main/java/pages/ChosenProductPage.java
@@ -13,8 +13,8 @@ public class ChosenProductPage extends BasePage{
     }
 
     public static final By ADD_TO_CART_BUTTON = By.id("add_to_cart");
-    public static final By CHECKOUT_BUTTON = By.xpath("//*[@class='button-container']//*[@title='Оформить заказ']");
-    public static final By CONTINUE_SHOPPING_BUTTON = By.xpath("//*[@class='button-container']//*[@title='Продолжить покупки']");
+    public static final By CHECKOUT_BUTTON = By.xpath("//*[contains(@class,'button-medium')]");
+    public static final By CONTINUE_SHOPPING_BUTTON = By.xpath("//*[contains(@class,'button exclusive-medium')]");
     public static final By TSHIRT_CATEGORY_BUTTON = By.xpath("//*[@title='T-shirts']");
 
 
@@ -28,7 +28,7 @@ public class ChosenProductPage extends BasePage{
 
     @Step("Clicking button 'Checkout'")
     public CartPage clickCheckoutButton(){
-        Waiters.waitForElementLocated(driver,By.xpath("//*[@class='button-container']//*[@title='Оформить заказ']"), 5);
+        Waiters.waitForElementLocated(driver,By.xpath("//*[contains(@class,'button-medium')]"), 5);
         log.info("Click 'Checkout' button");
         driver.findElement(CHECKOUT_BUTTON).click();
         return new CartPage(driver);
@@ -36,7 +36,7 @@ public class ChosenProductPage extends BasePage{
 
     @Step("Clicking button 'Continue'")
     public ChosenProductPage clickContinueShoppingButton(){
-        Waiters.waitForElementLocated(driver,By.xpath("//*[@class='button-container']//*[@title='Продолжить покупки']"), 5);
+        Waiters.waitForElementLocated(driver,By.xpath("//*[contains(@class,'button exclusive-medium')]"), 5);
         log.info("Click 'Continue' button");
         driver.findElement(CONTINUE_SHOPPING_BUTTON).click();
         return this;
